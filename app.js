f079220811f73f2030a769760809367cd4872344
Siddharth verma <990.siddharthverma@gmail.com>
2023-01-06 02:07:13 +0530

    feat: Final commit
    Done development

--- app.js
+++ app.js
@@ -2,9 +2,30 @@ var btnTranslate = document.querySelector("#btn-translate");
 var text = document.querySelector("#text");
 var outputDev = document.querySelector("#output");
 
+var ServerUrl = "https://api.funtranslations.com/translate/minion.json"
+
+ function getTranslation(text) {
+    return ServerUrl + "?" + "text=" + text
+ }  
+
+
+function errorHandler(error){
+    console.log("error Occurred", error)
+    alert("Something's Wrong, Please check");
+}
 
 function clickHandler() {
-    outputDev.innerText = "ajio da bananu " + text.value;
+    var getText = text.value;
+
+    fetch(getTranslation(getText))
+    .then(response => response.json())
+    .then(json => {
+        var translateText = json.contents.translated;
+
+        outputDev.innerText  = translateText
+    })
+        
+    .catch(errorHandler)
 };
 
 btnTranslate.addEventListener("click", clickHandler );
